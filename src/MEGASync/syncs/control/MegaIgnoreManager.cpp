19e8cd6164ad897ffd1d788bea3bfc10168a290e
Raphaël Lizé <rl@mega.nz>
2026-03-23 11:31:50 +0100

    MegaIgnoreManager: restore codec setting

--- src/MEGASync/syncs/control/MegaIgnoreManager.cpp
+++ src/MEGASync/syncs/control/MegaIgnoreManager.cpp
@@ -56,6 +56,7 @@ void MegaIgnoreManager::parseIgnoresFile()
         if (ignore.open(QIODevice::ReadOnly))
         {
             QTextStream in(&ignore);
+            in.setCodec("UTF-8");
             while (!in.atEnd())
             {
                 QString line = in.readLine();
@@ -313,6 +314,7 @@ MegaIgnoreManager::ApplyChangesError MegaIgnoreManager::applyChanges(bool update
         if (ignore.open(QIODevice::WriteOnly))
         {
             QTextStream out(&ignore);
+            out.setCodec("UTF-8");
             out << rules.join(QLatin1String("\n"));
 
             ignore.close();
